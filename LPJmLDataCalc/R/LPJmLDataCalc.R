6106c53d5e9b783eb8e50048879737e3317cbc2b
David Hoetten <davidho@LT1171.pik-potsdam.de>
2023-09-22 18:16:49 +0200

    added encapsulated units and arithmetic

--- LPJmLDataCalc/R/LPJmLDataCalc.R
+++ LPJmLDataCalc/R/LPJmLDataCalc.R
@@ -1,6 +1,6 @@
 #' @title LPJmLDataCalc
 #'
-#' @importFrom units set_units as_units drop_units
+#' @importFrom units set_units as_units drop_units deparse_unit
 #' @import lpjmlkit
 #'
 #' @description
@@ -17,7 +17,9 @@ LPJmLDataCalc <- R6::R6Class(
 
   public = list(
     initialize = function(data, meta_data=NULL) {
-      units::units_options(set_units_mode = "standard")
+      units::units_options(set_units_mode = "standard") # TODO: put this into apckage installation?
+      unitDataBasePath <- test_path("../../units", "udunits2.xml")
+      units::load_units_xml(unitDataBasePath) # TODO: install more LPJmL units? is this the right place to do this?
       super$initialize(data = data, meta_data=meta_data)
       if(methods::is(private$.meta, "LPJmLMetaData")){
         private$add_unit_from_meta_to_array()
@@ -41,6 +43,20 @@ LPJmLDataCalc <- R6::R6Class(
         stop("Expected an LPJmLDataCalc object")
       }
       private$.data = private$.data+lpj_calc_obj$.__data_with_unit__
+    },
+    mult_LPJmLDataCalc = function(lpj_calc_obj){
+      if(!inherits(lpj_calc_obj, "LPJmLDataCalc")){
+        stop("Expected an LPJmLDataCalc object")
+      }
+      private$.data = private$.data*lpj_calc_obj$.__data_with_unit__
+      private$copy_unit_from_array_to_meta()
+    },
+    divBy_LPJmLDataCalc = function(lpj_calc_obj){
+      if(!inherits(lpj_calc_obj, "LPJmLDataCalc")){
+        stop("Expected an LPJmLDataCalc object")
+      }
+      private$.data = private$.data/lpj_calc_obj$.__data_with_unit__
+      private$copy_unit_from_array_to_meta()
     }
   ),
 
@@ -48,6 +64,20 @@ LPJmLDataCalc <- R6::R6Class(
     add_unit_from_meta_to_array = function(){
       unit <- private$.meta$unit
       private$.data <- set_units(private$.data, as_units(unit))
+    },
+    copy_unit_from_array_to_meta = function(){
+      unit <- attr(self$.__data_with_unit__,"unit")
+      if(identical(unit$numerator , character(0)) ){ # TODO: use of identical correct?
+        numerator = 1
+      } else{
+        numerator = unit$numerator
+      }
+      denominator = unit$denominator
+      if(!identical(unit$denominator , character(0)) ){ # TODO: use of identical correct?
+        denominator = paste0("/", denominator)
+      }
+      unit_str <- paste0(numerator,denominator)
+      private$.meta$.__set_attribute__("unit", unit_str)
     }
   ),
 
@@ -73,4 +103,27 @@ LPJmLDataCalc <- R6::R6Class(
   objSum$add_LPJmLDataCalc(obj2)
   return(objSum)
 }
+
+#' @export
+`*.LPJmLDataCalc` = function(obj1,obj2) {
+  objSum <- obj1$clone(deep=TRUE)
+  objSum$mult_LPJmLDataCalc(obj2)
+  return(objSum)
+}
+
+#' @export
+`/.LPJmLDataCalc` = function(obj1,obj2) {
+  objSum <- obj1$clone(deep=TRUE)
+  objSum$divBy_LPJmLDataCalc(obj2)
+  return(objSum)
+}
+
+
+#' @export
+as_LPJmLDataCalc = function(obj) {
+  objCalc <- LPJmLDataCalc$new(NULL)
+  objCalc$feed_LPJmLData(obj)
+  return(objCalc)
+}
+
 
