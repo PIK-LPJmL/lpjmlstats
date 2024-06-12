c7996c0e64795a657ba238b1c779ab6a1ec77dce
Jannes <jannesbr@pik-potsdam.de>
2024-06-12 14:54:41 +0200

    fix linter errors, add test

--- tests/testthat/testplot_functions.R
+++ tests/testthat/testplot_functions.R
@@ -3,8 +3,11 @@ testthat::test_that("plot functions of metric run through", {
   baseline_dir <- testthat::test_path("../testdata/path1")
   under_test_dir <- testthat::test_path("../testdata/path2")
 
-  settings <-
-    list(soiln = list(GlobSumTimeAvgTable, GlobSumTimeseries, TimeAvgMap))
+  settings <- list(
+    soiln = list(
+      GlobSumTimeAvgTable, GlobSumTimeseries, TimeAvgMap, TimeAvgMapPlusBase
+    )
+  )
 
   out <-
     benchmark(baseline_dir, under_test_dir, settings, pdf_report = FALSE)
