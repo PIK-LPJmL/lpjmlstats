54c44f264bb6d4568ab047786eaa9d43d23570f5
Christoph Mueller <cmueller@pik-potsdam.de>
2024-11-06 14:46:49 +0100

    adding TimeAvgMapTreeCover metric also as a default

--- R/Metric_subclasses.R
+++ R/Metric_subclasses.R
@@ -691,3 +691,47 @@ GlobSumAnnTimeseriesFPC <- R6::R6Class( # nolint: object_name_linter.
     }
   )
 )
+
+#' @title TimeAvgMapTreeCover
+#' @description
+#' TimeAvgMapTreeCover metric
+#' @export
+TimeAvgMapTreeCover <- # nolint: object_name_linter.
+  R6::R6Class(
+    "TimeAvgMapTreeCover",
+    inherit = TimeAvgMapWithAbs,
+    public = list(
+
+      #' @description
+      #' add up bands for tree fpcs and plot them
+      #' @param data LPJmLDataCalc object to be summarized
+      summarize = function(data) {
+        bands <- data$meta$band_names
+        data <- subset(data, band = bands[grep("tree", bands)])
+        data$add_band("treecover_band", function(x) sum(x))
+        data <- aggregate(data, time = list(to = "sim_period", stat = "mean"))
+        data <- subset(data, band = "treecover_band")
+      },
+      m_options = list(
+        font_size = 4,
+        highlight = NULL,
+        quantiles = c(0.05, 0.95),
+        sep_cmp_lims = TRUE,
+        year_subset = as.character(1991:2000),
+        cell_subset = NULL,
+        n_breaks = 8,
+        num_cols = 3,
+        var_subheading = FALSE,
+        band_subheading = FALSE
+      ),
+      #' @field title
+      #' Section header used in the report
+      title = "Time Average Maps With Absolute Values of total Tree Cover",
+
+      #' @field description
+      #' Description used in the report
+      description = "The cell-time values of each variable are averaged over time.
+        Tree bands (currently 2:9) are added up.
+        This is then plotted on a map. \n"
+    )
+  )
