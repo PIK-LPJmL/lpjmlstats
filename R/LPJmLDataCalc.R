a3212a05f15af4c496987ac256fbe5f1d9c2daa7
Christoph Mueller <cmueller@pik-potsdam.de>
2026-07-27 12:43:13 +0200

    make CH4 unit conversion table matching robust and honor package unit-table option

--- R/LPJmLDataCalc.R
+++ R/LPJmLDataCalc.R
@@ -242,6 +242,17 @@ safe_set_units <- function(x, unit_str) {
   units::set_units(x, units::as_units(mapped_str))
 }
 
+# Canonicalize unit strings for robust lookup in conversion tables.
+# This resolves equivalent notations like yr-1, a-1, or /yr.
+normalize_unit_for_lookup <- function(unit_str) {
+  mapped_str <- map_units(unit_str, formula_to_internal)
+  normalized <- tryCatch(
+    units::deparse_unit(units::as_units(mapped_str)),
+    error = function(e) mapped_str
+  )
+  map_units(normalized, internal_to_formula)
+}
+
 # Copy the unit attribute from the meta data to the units data array
 LPJmLDataCalc$set("private", "copy_unit_meta2array",
                   function() {
@@ -308,9 +319,12 @@ LPJmLDataCalc$set(
     if (is.null(path_to_table)) {
       path_to_table <-
         getOption(
-          "LPJmLDataCalc.unit_conversion_table",
-          system.file("unit_conversions.csv",
-                      package = "lpjmlstats")
+          "lpjmlstats.unit_conversion_table",
+          getOption(
+            "LPJmLDataCalc.unit_conversion_table",
+            system.file("unit_conversions.csv",
+                        package = "lpjmlstats")
+          )
         )
     }
 
@@ -318,12 +332,16 @@ LPJmLDataCalc$set(
     conversions <- read.csv(path_to_table, stringsAsFactors = FALSE)
 
     # Get the current unit of the data array
-    current_unit <- units::deparse_unit(private$.data)
+    current_unit <- normalize_unit_for_lookup(units::deparse_unit(private$.data))
+    conversion_units <- vapply(conversions$original_unit,
+                               normalize_unit_for_lookup,
+                               character(1))
     # Check if current unit is in the table
-    if (current_unit %in% conversions$original_unit) {
+    if (current_unit %in% conversion_units) {
       # Get the corresponding converted unit
       converted_unit <-
-        conversions$converted_unit[conversions$original_unit == current_unit]
+        conversions$converted_unit[conversion_units == current_unit]
+      converted_unit <- converted_unit[[1]]
       # Convert the unit
       private$.__convert_unit__(converted_unit)
     }
